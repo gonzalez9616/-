5efcb20aa5e374a4dc81e54e16eb4b9962abd76e
gonzalez9616 <60353867+gonzalez9616@users.noreply.github.com>
2021-01-29 10:26:45 -0600

    Style

--- live-chat/js.js
+++ live-chat/js.js
@@ -1,31 +1,109 @@
+// Config | PLEASE DO NOT USE MY CONFIG
+var firebaseConfig = {
+    apiKey: "AIzaSyASOZNuboYfzw1CV2gOKavjiZnH4pzoiG0",
+    authDomain: "fir-basic-example.firebaseapp.com",
+    projectId: "fir-basic-example",
+    storageBucket: "fir-basic-example.appspot.com",
+    messagingSenderId: "933564106583",
+    appId: "1:933564106583:web:73ae94011620c244e12012",
+    measurementId: "G-VWCJSBXKSJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Request firestore
 let db = firebase.firestore()
-
-
-let user
-let displayData = {
-
-}
-let selectedChannel = 'main'
-const maxMessages = 10;
-
-fetchMessagesInit()
-
+//Request auth
 let provider = new firebase.auth.GoogleAuthProvider();
 
+//Setup vars
+let user
+let userData
+let displayData = {};
+let selectedChannel = 'main';
 
+//Const
+const maxMessages = 100; //Max number of messages to load
+const maxCombinedMessagesNum = 10; //The max number of messages that can be combined
+const maxTimeDifBetweenCombinedMessages = 60 * 5; //Max time between combined messages
 
-let userInputBox, displayUser, signInBlock, channelSelect
-
+//Define on load
+let userInputBox, displayUser, signInBlock, channelSelect, displayAllUsers, mainContentDiv
 function onLoad() {
     displayUser = document.getElementById('displayUser')
     signInBlock = document.getElementById('signInBlock')
     userInputBox = document.getElementById('messageInputBox')
     channelSelect = document.getElementById('channelSelect')
+    displayAllUsers = document.getElementById('displayAllUsers')
+    mainContentDiv = document.getElementsByClassName("mainContent")[0]
+
+    //To add focus to typing if not typing in correct box
+    document.addEventListener("keydown", function(key) {
+        if (document.body === document.activeElement) {
+            //Change focus if regular key
+            if (key.code.includes("Key")) {
+                //Random stackoverflow function because focusing at the end of the text is such a huge issue ahghghghgh
+                function placeCaretAtEnd(el) {
+                    if (typeof window.getSelection != "undefined"
+                        && typeof document.createRange != "undefined") {
+                        var range = document.createRange();
+                        range.selectNodeContents(el);
+                        range.collapse(false);
+                        var sel = window.getSelection();
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                    } else if (typeof document.body.createTextRange != "undefined") {
+                        var textRange = document.body.createTextRange();
+                        textRange.moveToElementText(el);
+                        textRange.collapse(false);
+                        textRange.select();
+                    }
+                }
+
+                userInputBox.focus();
+                placeCaretAtEnd(userInputBox);
+            }
+        }
+    })
+
     userInputBox.addEventListener("keydown", function(key) {
-        if (key && key.key === 'Enter') {
+        if (key && key.key === 'Enter' && !key.getModifierState('Shift')) {
             sendMessage()
         }
     })
+
+    mainContentDiv.addEventListener("scroll", function() {
+        lastPos = mainContentDiv.scrollTop
+        scrollingWithContent = (mainContentDiv.scrollTop) > (mainContentDiv.scrollHeight - mainContentDiv.offsetHeight - 15)
+    })
+    scrollDown()
+    fixDumbCss()
+}
+
+function fixDumbCss() {
+    let baseContentDiv = document.getElementsByClassName("baseDiv")[0]
+    let topContentDiv = document.getElementsByClassName("topContent")[0]
+    let centerContentDiv = document.getElementsByClassName("centerContent")[0]
+    centerContentDiv.style.height = (baseContentDiv.clientHeight - topContentDiv.clientHeight) + "px"
+
+    window.requestAnimationFrame(fixDumbCss)
+}
+
+let scrollingWithContent = true, lastPos = 0
+function scrollDown() {
+    if (scrollingWithContent) {
+        window.requestAnimationFrame(function() {
+            mainContentDiv.scrollTo(0, mainContentDiv.scrollHeight)
+        })
+    } else {
+        mainContentDiv.scrollTo(0, lastPos)
+    }
+}
+
+function removeSignInBlock() {
+    displayUser.innerText = "Signed In As: " + user.displayName
+    signInBlock.style.display = 'none'
+    displayUser.style.display = 'block'
 }
 
 function userSignIn() {
@@ -35,33 +113,100 @@ function userSignIn() {
             //Worked
             user = result.user;
 
-            displayUser.innerText = "Signed In As: " + user.displayName
-            signInBlock.style.display = 'none'
-            displayUser.style.display = 'block'
-            displayMessagesData()
+            let doc = db.collection("users").doc(user.uid)
+
+            doc.get().then((snapshot => {
+                if (snapshot.exists) {
+                    //Already has user data
+                    userData = snapshot.data();
+                    removeSignInBlock()
+                } else {
+                    //Need to make user data
+                    let newData = {
+                        displayName: user.displayName,
+                        displayImage: user.photoURL,
+                        status: 0,
+                        motd: "",
+                        admin: false,
+                    }
+                    doc.set(newData).then(() => {
+                        userData = newData
+                        removeSignInBlock()
+                    }).catch(issue => {
+                        console.log("Throttling issue likely: " + issue)
+                    })
+                }
+            })).catch(issue => {
+                console.log("Throttling issue likely: " + issue)
+            })
+
+            fetchMessagesInit()
+            fetchUsersInit()
         }).catch((error) => {
-            //Failed
-            console.log("Failed auth: " + error.code)
-            console.log(error)
-        });
+        //Failed
+        console.log("Failed auth: " + error.code)
+        console.log(error)
+    });
 
 }
 
 function sendMessage() {
-    let userInput = userInputBox.value
+    let userInput = userInputBox.innerText
     if (!user) {
         alert('Sign in first')
-    } else if (userInput.length > 0) {
+    } else if (userInput.length > 0 && userInput.trim().length > 0) {
         let messages = db.collection("channels").doc(selectedChannel).collection("messages")
         messages.add({
             created: firebase.firestore.FieldValue.serverTimestamp(),
             content: userInput,
             uId: user.uid,
             displayName: user.displayName,
+            displayImage: user.photoURL,
         })
-
-        userInputBox.value = ""
     }
+
+    userInputBox.innerText = ""
+
+    setTimeout(function() {
+        userInputBox.innerText = "" //Fix weird issue that chrome has with whitespace, idk why
+    }, 0)
+}
+
+
+function displayUserSnapshot(snapshot) {
+    displayAllUsers.innerHTML = ""
+    snapshot.forEach((doc) => {
+        let data = doc.data()
+        let div = document.createElement("div")
+
+        let height = 25;
+
+        let img = document.createElement("img")
+        img.src = data.displayImage;
+        img.width = height;
+        img.height = height;
+        img.style.float = "left"
+
+        let p = document.createElement("p")
+        p.innerText = data.displayName
+        p.style.fontSize = height + "px"
+        p.style.float = "left"
+        p.style.margin = '0px';
+        p.style.paddingLeft = "8px"
+
+        div.append(img, p)
+        displayAllUsers.appendChild(div)
+    })
+}
+
+async function fetchUsersInit() {
+    let users = db.collection("users").orderBy("displayName", "asc").limit(100)
+    let usersSnapshot = await users.get()
+    displayUserSnapshot(usersSnapshot)
+
+    users.onSnapshot((snapshot) => {
+        displayUserSnapshot(snapshot)
+    })
 }
 
 async function fetchMessagesInit() {
@@ -69,19 +214,28 @@ async function fetchMessagesInit() {
     let channelsSnapshot = await channels.get()
 
     channelsSnapshot.forEach((data) => {
-        let option = document.createElement("option")
-        option.innerText = data.id;
-        option.value = data.id;
+        let option = document.createElement("button")
+        let id = data.id;
+        option.innerText = id;
+        option.onclick = function() {
+            selectedChannel = id;
+            swapSelectedChannel()
+        }
         channelSelect.appendChild(option)
     })
 
-    channelSelect.value = selectedChannel
+    selectedChannel = "main";
 
     swapSelectedChannel()
+    displayMessagesData()
 }
 
 function swapSelectedChannel() {
-    selectedChannel = channelSelect.value;
+    if (displayData) {
+        if (displayData.snapshot) {
+            displayData.snapshot()
+        }
+    }
 
     let thisChannel = selectedChannel
 
@@ -90,31 +244,24 @@ function swapSelectedChannel() {
 
     let query = messages.orderBy("created", "desc").limit(maxMessages + 1); //most recent message first, then the last x
 
-    if (!displayData[selectedChannel]) {
-        displayData[selectedChannel] = {
-            data: null,
-            onsnapshot: false,
-        }
-        query.get() //First get to catch up
-            .then((snapshot) => {
-                displayMessageSnapshot(snapshot, thisChannel)
-            })
-
+    displayData = {
+        data: null,
+        snapshot: null,
     }
-
-    if (!displayData[thisChannel].onsnapshot) {
-        displayData[thisChannel].onsnapshot = true
-
-        query.onSnapshot((snapshot) => {
+    query.get() //First get to catch up
+        .then((snapshot) => {
             displayMessageSnapshot(snapshot, thisChannel)
         })
-    }
 
-    displayMessagesData()
+
+    displayData.snapshot = query.onSnapshot((snapshot) => {
+        displayMessageSnapshot(snapshot)
+    })
+
+    lastHeight = 0
 }
 
-function displayMessageSnapshot(snapshot, channel) {
-
+function displayMessageSnapshot(snapshot) {
     let messages = []
     snapshot.forEach(function(doc) {
         let data = doc.data()
@@ -127,6 +274,7 @@ function displayMessageSnapshot(snapshot, channel) {
             messages.splice(0, 0, {
                 content: asString,
                 uId: data.uId,
+                seconds: data.created.seconds,
             })
         }
     });
@@ -135,12 +283,12 @@ function displayMessageSnapshot(snapshot, channel) {
         messages.shift()
     }
 
-    displayData[channel].data = messages
+    displayData.data = messages
     displayMessagesData()
 }
 
 function displayMessagesData() {
-    let messages = displayData[selectedChannel] && displayData[selectedChannel].data
+    let messages = displayData && displayData.data
 
     if (!messages) {
         return
@@ -149,21 +297,39 @@ function displayMessagesData() {
     let displayMessages = document.getElementById('displayMessages')
     displayMessages.innerHTML = ""
 
+    let lastData
     messages.forEach(function(value) {
-        let div = document.createElement("div")
-        let p = document.createElement("p")
-
-        p.setAttribute("class", "messageContent")
-        if (user && value.uId === user.uid) {
-            div.setAttribute("class", "ourMessage")
-            p.style.backgroundColor = "#454545"
+        //Checks for criteria in which to combine messages by the same person if the time combined & number of combined
+        //is within constants (makes chat nicer)
+        if (lastData && lastData.uId === value.uId &&
+            (value.seconds - lastData.begin) < maxTimeDifBetweenCombinedMessages &&
+            lastData.num < maxCombinedMessagesNum)
+        {
+            lastData.num++;
+            lastData.p.innerText = lastData.p.innerText + "\n" + value.content
         } else {
-            div.setAttribute("class", "message")
-            p.style.backgroundColor = "#3b3b3b"
-        }
-        p.innerText = value.content
+            let div = document.createElement("div")
+            let p = document.createElement("p")
 
-        div.appendChild(p)
-        displayMessages.appendChild(div)
+            p.setAttribute("class", "messageContent")
+            div.setAttribute("class", "message")
+            p.innerText = value.content
+
+            div.appendChild(p)
+            displayMessages.appendChild(div)
+
+
+            lastData = {
+                p: p,
+                uId: value.uId,
+                num: 1,
+                begin: value.seconds,
+            }
+        }
     })
+    scrollDown()
+}
+
+function displayNewMessage() {
+
 }
